f4fb881d47da108c81e6c30d7faa75860eda71e2
Suddhu2004 <suddhu040804@gmail.com>
2023-06-15 14:41:59 +0530

    Loading Added

--- loader.js
+++ loader.js
@@ -1,19 +1,6 @@
-$(document).ready(function() {
-	// Users can skip the loading process if they want.
-	$('.skip').click(function() {
-		$('.overlay, body').addClass('loaded');
-	})
-	
-	// Will wait for everything on the page to load.
-	$(window).bind('load', function() {
-		$('.overlay, body').addClass('loaded');
-		setTimeout(function() {
-			$('.overlay').css({'display':'none'})
-		}, 2000)
-	});
-	
-	// Will remove overlay after 1min for users cannnot load properly.
+$(window).on('load', function() {
 	setTimeout(function() {
-		$('.overlay, body').addClass('loaded');
-	}, 60000);
-})
+	  $('#loading-window').fadeOut('slow');
+	}, 30000); // 30 seconds in milliseconds
+  });
+  
